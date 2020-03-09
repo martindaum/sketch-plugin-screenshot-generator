ed0a33fe37e40a8335cb384b0dbbe75f0f84150c
Martin Daum <office@martindaum.com>
2020-03-09 11:16:31 +0100

    Improve plugin

--- src/generate.js
+++ src/generate.js
@@ -1,66 +1,108 @@
-var sketch = require('sketch')
-var UI = require('sketch/ui')
+const sketch = require('sketch')
+const UI = require('sketch/ui')
 
-var fileManager = NSFileManager.defaultManager()
-var supportedImages = NSArray.arrayWithArray(["png", "jpg", "jpeg"])
+const fileManager = NSFileManager.defaultManager()
+const supportedImages = NSArray.arrayWithArray(["png", "jpg", "jpeg"])
 var outputFolder = ""
 
+export default function(context) {
+	const documentURL = context.document.fileURL()
+	const folderURL = documentURL.URLByDeletingLastPathComponent()
+	const manifestURL = folderURL.URLByAppendingPathComponent("screenshots.manifest.json")
+	if (fileManager.fileExistsAtPath(manifestURL.path())) {
+		readManifest(manifestURL)
+	} else {
+		askForManifestFile()
+	}
+}
+
 function jsonFromFile(filePath) {
-	var data = NSData.dataWithContentsOfFile(filePath)
+	const data = NSData.dataWithContentsOfFile(filePath)
 	return NSJSONSerialization.JSONObjectWithData_options_error(data, 0, nil)
 }
 
-function processLanguage(language, url) {
+function processLanguage(language, url) {	
 	// strings
-	var stringsFile = url.URLByAppendingPathComponent(language["strings"])
-	var strings = jsonFromFile(stringsFile)
-	var keys = strings.allKeys()
-	keys.forEach(key => {
-    	replaceText(key, strings[key])
-  	})
+	if (language["strings"] != undefined) {
+		const stringsFile = url.URLByAppendingPathComponent(language["strings"])
+		if (!fileManager.fileExistsAtPath(stringsFile.path())) {
+			UI.message("⚠️ Language file not found!")
+			return
+		}
+		processTexts(stringsFile)
+	}
+
+	// screenshots
+	if (language["screenshots"] != undefined) {
+		const imageFolder = url.URLByAppendingPathComponent(language["screenshots"])
+		processImages(imageFolder)
+	}
 
 	// images
-	var imageFolder = url.URLByAppendingPathComponent(language["screenshots"])
-	var contents = fileManager.contentsOfDirectoryAtPath_error(imageFolder.path(), nil)
-	contents.forEach(file => {
-		var fileName = file.lastPathComponent()
-    	var pathExtension = fileName.pathExtension()
-    
-    	if(supportedImages.containsObject(pathExtension)) {
-      		var imageURL = imageFolder.URLByAppendingPathComponent(file)
-      		replaceImage(fileName.stringByDeletingPathExtension(), imageURL)
-    	}
-	})
+	if (language["images"] != undefined) {
+		const imageFolder = url.URLByAppendingPathComponent(language["images"])
+		processImages(imageFolder)
+	}
 
 	exportArtboards(language, url)
 }
 
-function replaceImage(key, url) {
-  var selector = '[name="{{{' + key + '}}}"]'
-  var imageLayers = sketch.find(selector)
-  imageLayers.forEach(layer => {
-      layer.overrides.forEach(override => {
-        if (override.property !== 'image') {
-          return
-        }
+function processTexts(file) {
+	const strings = jsonFromFile(file)
+	const keys = strings.allKeys()
+	keys.forEach(key => {
+    	replaceText(key, strings[key])
+  	})
+}
 
-        var srcImage = NSImage.alloc().initByReferencingFile(url.path())
-        override.value = srcImage
-      })
+function processImages(folder) {
+	const contents = fileManager.contentsOfDirectoryAtPath_error(folder.path(), nil)
+	contents.forEach(file => {
+		const fileName = file.lastPathComponent()
+    	const pathExtension = fileName.pathExtension()
+    
+    	if(supportedImages.containsObject(pathExtension)) {
+      		const imageURL = folder.URLByAppendingPathComponent(file)
+      		replaceImage(fileName.stringByDeletingPathExtension(), imageURL)
+    	}
+	})
+}
+
+function replaceImage(key, url) {
+  const selector = '[name="[' + key + ']"]'
+  const imageLayers = sketch.find(selector)
+  imageLayers.forEach(layer => {
+  	if (layer.type == 'Image' && layer.image != undefined) {
+		const srcImage = NSImage.alloc().initByReferencingFile(url.path())
+		layer.image = srcImage
+  	} else if (layer.overrides != undefined) {
+  		layer.overrides.forEach(override => {
+			if (override.property !== 'image') {
+			  return
+			}
+
+			const srcImage = NSImage.alloc().initByReferencingFile(url.path())
+			override.value = srcImage
+		})
+  	}
   })  
 }
 
 function exportArtboards(language) {
-	var outputs = language["output"]
-	var exportFolder = outputFolder.URLByAppendingPathComponent(outputs[0])
-  	var pages = sketch.getSelectedDocument().pages
+	const outputs = language["output"]
+	const exportFolder = outputFolder.URLByAppendingPathComponent(outputs[0])
+  	const pages = sketch.getSelectedDocument().pages
   	pages.forEach(page => {
   		if (page.isSymbolsPage()) {
   			return
   		}
 
-	  	var layers = page.layers
+	  	const layers = page.layers
 	  	layers.forEach(layer => {
+	  		// only export layers with exportFormat assigned
+	  		if (layer.exportFormats.length <= 0) {
+	  			return
+	  		}
 		  	const options = { formats: 'png', output: exportFolder.path(), overwriting: true }
 		  	sketch.export(layer, options)
 		})
@@ -71,6 +113,10 @@ function exportArtboards(language) {
   	}
 
   	UI.message("All done! 🚀")
+}
+
+function cleanOutputFolder() {
+	fileManager.removeItemAtPath_error(outputFolder.path(), nil)
 }
 
 function copyOutputFolder(outputs) {
@@ -86,25 +132,39 @@ function copyOutputFolder(outputs) {
 }
 
 function replaceText(key, text) {
-  var selector = 'Text, [name="{{{' + key + '}}}"]'
-  var textLayers = sketch.find(selector)
+  const selector = 'Text, [name="[' + key + ']"]'
+  const textLayers = sketch.find(selector)
   textLayers.forEach(layer => {
       layer.text = text
   })
 }
 
 function readManifest(url) {
-	var json = jsonFromFile(url)
-	var baseURL = url.URLByDeletingLastPathComponent()
+	const json = jsonFromFile(url)
+	const baseURL = url.URLByDeletingLastPathComponent()
+
+	if (json["output"] == undefined) {
+		UI.message("⚠️ No output folder defined")
+		return
+	}
+
 	outputFolder = baseURL.URLByAppendingPathComponent(json["output"])
-	var languages = json["languages"]
+	cleanOutputFolder()
+
+	// process languages
+	if (json["languages"] == undefined || json["languages"].length <= 0) {
+		UI.message("⚠️ No languages defined")
+		return
+	}
+
+	const languages = json["languages"]
 	languages.forEach(language => {
     	processLanguage(language, baseURL)
   	})
 }
 
-function openFolder() {
-	var openDialog = NSOpenPanel.openPanel()
+function askForManifestFile() {
+	const openDialog = NSOpenPanel.openPanel()
 	openDialog.setCanChooseFiles(true)
 	openDialog.setAllowedFileTypes(["json"])
 	openDialog.setCanChooseDirectories(false)
@@ -115,8 +175,3 @@ function openFolder() {
 		readManifest(openDialog.URL())
 	}
 }
-
-export default function() {
-	openFolder()
-}
-
